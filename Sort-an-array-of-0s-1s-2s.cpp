abc3a5ba828ff539f5ef0c03222a007b45fb7e4c
Deox <70789919+dateduker@users.noreply.github.com>
2021-08-19 16:34:07 +0530

    Update Sort-an-array-of-0s-1s-2s.cpp

--- Sort-an-array-of-0s-1s-2s.cpp
+++ Sort-an-array-of-0s-1s-2s.cpp
@@ -25,24 +25,16 @@ Method 1
     int lo = 0;
     int hi = arr_size - 1;
     int mid = 0;
- 
-    // Iterate till all the elements
-    // are sorted
+
     while (mid <= hi) {
         switch (a[mid]) {
- 
-        // If the element is 0
-        case 0:
+         case 0:
             swap(a[lo++], a[mid++]);
             break;
- 
-        // If the element is 1 .
-        case 1:
+         case 1:
             mid++;
             break;
- 
-        // If the element is 2
-        case 2:
+         case 2:
             swap(a[mid], a[hi--]);
             break;
         }
